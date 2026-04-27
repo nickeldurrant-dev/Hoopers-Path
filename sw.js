a87a7aaa66ee63db9d958c2ce7b61bcf341c9464
nickeldurrant-dev <nickeldurrant@gmail.com>
2026-04-27 09:27:45 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Hooper's Path service worker - network-first for HTML, cache-first for assets
-const CACHE_NAME = 'hoopers-path-v25';
+const CACHE_NAME = 'hoopers-path-v26';
 const ASSETS = [
   './',
   './index.html',
